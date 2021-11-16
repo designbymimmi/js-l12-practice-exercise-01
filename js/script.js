e96272d17968c740e27aa96f7836f05c4843513f
designbymimmi <designbymimmi@gmail.com>
2021-11-16 11:34:55 +1000

    Adding random user data to dislay on site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+  let usersRequest = await fetch("https://randomuser.me/api/?results=5");
+  let data = await usersRequest.json();
+  let userResults = data["results"];
+  // console.log(userResults);
+  // console.log(displayUsers(userResults));
+  displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+  randomFolks.innerHTML = "";
+  for (let user of userResults) {
+    let country = user["location"]["country"];
+    let name = user["name"]["first"];
+    let imageURL = user["picture"]["medium"];
+    // console.log(country, name, imageURL);
+    let userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageURL} alt="User avatar" />
+    `;
+    randomFolks.append(userDiv);
+  }
+};
+
+getData();
